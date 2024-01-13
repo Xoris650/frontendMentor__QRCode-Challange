4a27f1b3765aa277c38a37987e709e3cf1270ab2
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-01-13 19:40:23 -0300

    Ultrawide compatibility

---
+++ src/script/script.js
@@ -0,0 +1,7 @@
+function expandImageText(visibility){
+    document.getElementById("expand_image").style.visibility = visibility
+}
+
+function expandImage(){
+    console.log("Expandindo Imagem")
+}
