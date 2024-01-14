0f02ebb87975d6c91d100ad1b1bf292964b614b6
Gabriel Evaristo <gabrielousadia12@gmail.com>
2024-01-14 02:40:11 -0300

    Start popup functions

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,19 @@
-function expandImageText(visibility){
-    document.getElementById("expand_image").style.visibility = visibility
+function expandImageText(){
+    document.getElementById("QRCode").style.filter = "blur(4px)"
+    document.getElementById("expand_image").style.visibility = "visible"
 }
 
-function expandImage(){
-    console.log("Expandindo Imagem")
+function undoExpandImageText(){
+    document.getElementById("QRCode").style.filter = "blur(0px)"
+    document.getElementById("expand_image").style.visibility = "hidden"
+}
+
+function openPopUp(){
+    var popup = document.getElementById("popup")
+    popup.style.display = "flex"
+    popup.style.animation = ".7s ease 0s 1 normal forwards running popupAnimation"
+
+}
+function closePopUp(){
+    document.getElementById("popup").style.display = "none"
 }
